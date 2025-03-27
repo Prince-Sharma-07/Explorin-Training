9286a274dde14f1247dc9688238c73329b33c22b
Prince sharma <connect.princesharma@gmail.com>
2025-03-27 10:31:18 +0530

    js day5

---
+++ JavaScript/Day5/script.js
@@ -0,0 +1,23 @@
+var a
+var b = 20
+
+console.log(abc)
+
+console.log(a)
+
+a = 20
+
+function abc(){
+    var g = 55
+    var h = 40
+
+    function x(){
+            console.log('x executed')
+    }
+    function y() {
+        console.log('y executed')
+    }
+    x()
+    y()
+}
+
